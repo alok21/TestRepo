7c480466e48d6a5138f453400b28bb9add938340
alokd <alokd@alokd.dbiwin2k3.domain.local>
2013-08-28 12:39:12 +0530

    Changes to test git sparse checkout

--- _GitProject/src/org/dstadler/jgit/porcelain/CloneRemoteRepository.java
+++ _GitProject/src/org/dstadler/jgit/porcelain/CloneRemoteRepository.java
@@ -2,8 +2,10 @@ package org.dstadler.jgit.porcelain;
 
 import java.io.File;
 
+import org.dstadler.jgit.helper.CookbookHelper;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.StoredConfig;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
 
@@ -14,7 +16,7 @@ import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
  * @author dominik.stadler@gmx.at
  */
 public class CloneRemoteRepository {
-	private static final String REMOTE_URL = "https://github.com/github/testrepo.git";
+	private static final String REMOTE_URL = "E:\\GitRepo\\GitRepo\\.git";
 
 	public static void main(String[] args){
 		try
@@ -22,18 +24,21 @@ public class CloneRemoteRepository {
 			// prepare a new folder for the cloned repository
 			File localPath = File.createTempFile("TestGitRepository", "");
 			localPath.delete();
-
-			// then clone
+			//Repository repository = CookbookHelper.openJGitCookbookRepository();
+			
 			System.out.println("Cloning from " + REMOTE_URL + " to " + localPath);
+			
 			Git.cloneRepository().setURI(REMOTE_URL).setDirectory(localPath).call();
-
+			//Git.cloneRepository().setBranch(REMOTE_URL).setDirectory(localPath).call();
 			// now open the created repository
 			FileRepositoryBuilder builder = new FileRepositoryBuilder();
 			Repository repository =
 				builder.setGitDir(localPath).readEnvironment() // scan environment GIT_* variables
 						.findGitDir() // scan up the file system tree
 						.build();
-
+//			StoredConfig config = repository.getConfig();
+//			config.setBoolean("core", null , "sparsecheckout", true);
+			
 			System.out.println("Having repository: " + repository.getDirectory());
 
 			repository.close();
